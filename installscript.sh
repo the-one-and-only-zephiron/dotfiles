05b3b61a619153331754587c7c147b4790bb8bee
the-one-and-only-zephiron <87841019+the-one-and-only-zephiron@users.noreply.github.com>
2022-03-20 13:07:09 +0000

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "updating the system and installing basic packages..."
-sudo pacman -Syu --noconfirm base-devel vim feh xorg xorg-drivers xorg-xinit i3-gaps alacritty pulseaudio fish 
+sudo pacman -Syu --noconfirm base-devel vim feh xorg xorg-drivers xorg-xinit i3-gaps alacritty pulseaudio fish firefox
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git/
 makepkg -si --noconfirm 
@@ -8,7 +8,7 @@ cd
 sudo rm -r yay-git/
 clear
 echo "installing AUR packages..."
-yay -Syu --noconfirm polybar picom-ibhagwan-git librewolf-bin nerd-fonts-complete starship ranger ttf-icomoon-feather
+yay -Syu --noconfirm polybar picom-ibhagwan-git nerd-fonts-complete starship ranger ttf-icomoon-feather
 clear
 echo "copying your dotfiles..."
 cp .alacritty.yml .xinitrc ~/ 
